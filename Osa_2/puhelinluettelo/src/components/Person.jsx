9bb5b5a1f02d76c87e32d9d3077bf5deacaa3df6
nie-ed <70572189+nie-ed@users.noreply.github.com>
2023-09-12 16:31:28 +0300

    Delete Osa_2 directory

--- Osa_2/puhelinluettelo/src/components/Person.jsx
+++
@@ -1,9 +0,0 @@
-const Person = ({person, deletePerson}) => {
-    return (
-      <li>{person.name} {person.number}
-      <button onClick={deletePerson}>delete</button>
-      </li>
-    )
-  }
-
-  export default Person
