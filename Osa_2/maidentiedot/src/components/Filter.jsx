b96d558215f547c0e3fdc24a44e0554bc413b5f8
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2023-09-12 15:20:32 +0300

    Osa 2

---
+++ Osa_2/maidentiedot/src/components/Filter.jsx
@@ -0,0 +1,13 @@
+const Filter = (props) => {
+    return (
+      <div>
+        find countries
+        <input
+        value={props.newFilter}
+        onChange={props.handleFilterChange}/>
+      </div>
+      )
+  }
+
+  
+  export default Filter
